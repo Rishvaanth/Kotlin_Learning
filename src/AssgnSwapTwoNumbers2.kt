6ac95f91c23b7083a9188bc20d95173e1f6e1786
rishvaanth13 <Rishvaanth.T@ril.com>
2020-02-26 20:09:37 +0530

    Assignment 2 Swap two numbers without using a third variable
    Signed-off-by: rishvaanth13 <Rishvaanth.T@ril.com>

---
+++ src/AssgnSwapTwoNumbers2.kt
@@ -0,0 +1,10 @@
+/*      Given two variables, x and y, swap two variables without using a third variable.        */
+fun main(){
+    var x = 10
+    var y = 20
+    println("Numbers before swapping are $x , $y")
+    x += y
+    y = x - y
+    x -= y
+    println("Numbers after swapping are $x , $y")
+}
