665f2598b1fb98ddb5ce5a560a994c67477991da
rishvaanth13 <Rishvaanth.T@ril.com>
2020-02-26 19:19:47 +0530

    Initial commit

---
+++ src/New.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var str:String? = null
+
+    println(str)
+    println(str)
+    println(str)
+
+//    str = "Hello I am learning for so long!"
+
+    println ("Welcome to Kotlin")
+    print ("Hello")
+    println("Im learning yet again")
+
+    println("The string is ${str!!}")
+
+
+}
