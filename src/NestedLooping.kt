647ea128ab3879be2bc26e208da8e8115452e60b
rishvaanth13 <Rishvaanth.T@ril.com>
2020-02-27 12:56:21 +0530

    Nested Loops
    Signed-off-by: rishvaanth13 <Rishvaanth.T@ril.com>

--- src/NestedLooping.kt
+++ src/NestedLooping.kt
@@ -1,16 +1,30 @@
 fun main(){
-    print("Enter your name:")
-    val name = readLine()!!.toString()
-    print("Enter your Town/City where you live:")
-    val city = readLine()!!.toString()
-    var pets: String? = null
-    for (i in 1..3){
-        println("Enter pet $i name:")
-        pets = pets+ "," + readLine().toString()
+    val defaultString= "No Users Registered"
+    print("Enter number of Users:")
+    val numberOfUsers = readLine()!!.toInt()
+    if (numberOfUsers == 0) {
+        println(defaultString)
+        return
+    } else {
+        for (u in 1..numberOfUsers) {
+            print("Enter user $u's name:")
+            val name = readLine()!!.toString()
+            print("Enter Town/City where $name lives:")
+            val city = readLine()!!.toString()
+            var pets: String? =""
+            print("Enter how many pets User $name has:")
+            val numberOfPets = readLine()!!.toInt()
+            for (i in 1..numberOfPets) {
+                println("Enter pet $i name:")
+                pets = pets + readLine().toString()+","
+            }
+            println("<=========User Info==========>")
+            println("User name is: $name")
+            println("User Lives in: $city")
+            println("User Has: $pets")
+            if (pets!!.contains("cat")) println("We have a program for cat for $name!")
+            println("<=========End of User Info==========>")
+        }
     }
-    println("<=========User Info==========>")
-    println("User name is: $name")
-    println("User Lives in: $city")
-    println("User Has: $pets")
-
+    println("<=========End of Program===========>")
 }
