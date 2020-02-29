c38feae2261cf30c4870b8a12317841568621826
rishvaanth13 <Rishvaanth.T@ril.com>
2020-02-29 20:44:20 +0530

    Read File
    Signed-off-by: rishvaanth13 <Rishvaanth.T@ril.com>

--- src/Files.kt
+++ src/Files.kt
@@ -1,10 +1,18 @@
+import java.io.FileReader
 import java.io.FileWriter
 import java.lang.Exception
 
 fun main(){
-    print("Write to File Text here:")
-    val x = readLine().toString()
-    writeToFile(x)
+    print("1 to Read, 2 to Write:")
+    when (readLine()!!.toInt()){
+        1->readFromFile()
+        2->{
+            print("Write to File Text here:")
+            val x = readLine().toString()
+            writeToFile(x)
+        }
+    }
+
 }
 
 fun writeToFile(str:String){
@@ -16,5 +24,20 @@ fun writeToFile(str:String){
     }catch (e:Exception){
         println(e.message)
         println("File has not been created")
+    }
+}
+
+
+fun readFromFile(){
+    try {
+        val file = FileReader("text.txt")
+        var c:Int
+        do {
+            c = file.read()
+            print(c.toChar())
+        }while (c!=-1)
+    }catch (e:Exception){
+        println(e.message)
+        println("File Read Failed")
     }
 }
